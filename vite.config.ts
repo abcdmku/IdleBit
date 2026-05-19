492527cce2051fa757d5c17e89a0ebbd36ce5371
abcdmku <63693423+abcdmku@users.noreply.github.com>
2026-05-18 21:29:38 -0500

    Github Action for Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 const host = "127.0.0.1";
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "IdleBit";
+const base = process.env.GITHUB_PAGES === "true" ? `/${repositoryName}/` : "/";
 
 export default defineConfig({
+  base,
   plugins: [react()],
   server: {
     host,
